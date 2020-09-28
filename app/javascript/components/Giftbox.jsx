a6a9a22773d8662d6136816eae531c54b65ef26f
David Anderson <d.c.anderson@live.com>
2020-09-28 09:07:34 -0400

    Pushing for help.

--- app/javascript/components/Giftbox.jsx
+++ app/javascript/components/Giftbox.jsx
@@ -1,2 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+class Giftbox extendes React.component {
+    constructor(props) {
+        super(props);
+        this.state = { giftbox: { traits: ""} };
+
+        this.addHtmlEntitities = this.addHtmlEntities.bind(this);
+    }
+}
+
+export default Recipe;
